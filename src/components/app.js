220a901f4ee2e9826f590abd706bc136e8cc8b91
Otto Peräkylä <otto@Ottos-MacBook-Pro.local>
2017-08-26 13:01:37 +0300

    initial

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import GoogleMap from './GoogleMap'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {lat:-34.397, lng: 150.644}
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div style={{height: '100%'}}>
+        <p>Map me</p>
+        <button onClick={() => this.setState({lat: 40.7128, lng: -74.0059 })}>
+          New York
+        </button>
+        <GoogleMap lat={this.state.lat} lng={this.state.lng} />
+      </div>
     );
   }
-}
+} 
+ 
